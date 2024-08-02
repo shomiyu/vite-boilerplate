a07146a23be799d370c60915649042d0539ec96a
shomiyu <m1011522@gmail.com>
2024-08-03 01:54:11 +0900

    エイリアスの設定

--- vite.config.js
+++ vite.config.js
@@ -61,7 +61,14 @@ export default defineConfig({
     }),
   ],
 
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+
   server: {
+    host: "0.0.0.0",
     port: 3000,
     open: true,
   },
