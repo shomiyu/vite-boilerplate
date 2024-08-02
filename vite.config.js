9349b9cac22bbd5fbc744c7c663ff8ac3a36493d
shomiyu <m1011522@gmail.com>
2024-08-03 01:37:47 +0900

    出力の設定

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,68 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import globule from "globule";
+import viteImagemin from "vite-plugin-imagemin";
 
+// ディレクトリ
+const dir = {
+  src: "src",
+  publicDir: "../public",
+  outDir: "../dist",
+};
+
+// マルチページ
+const inputs = {};
+const documents = globule.find([`./${dir.src}/**/*.html`], {
+  ignore: [`./${dir.src}/**/_*.html`],
+});
+documents.forEach((document) => {
+  const fileName = document.replace(`./${dir.src}/`, "");
+  const key = fileName.replace("index.html", "main").replace("/main", "");
+
+  inputs[key] = resolve(__dirname, dir.src, fileName);
+});
+
+// ----------------------------------------------
+// 設定
+// - https://vitejs.dev/config/
+// ----------------------------------------------
 export default defineConfig({
-  root: "src",
+  root: dir.src,
+  publicDir: dir.publicDir,
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   build: {
-    outDir: "../dist",
+    outDir: dir.outDir,
     emptyOutDir: true,
     rollupOptions: {
-      input: ["src/index.html", "src/html/sample.html"],
+      input: { ...inputs },
       output: {
         entryFileNames: `assets/js/[name].js`,
         chunkFileNames: `assets/js/[name].js`,
