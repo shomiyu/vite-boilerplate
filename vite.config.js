0b77b712d4d5fac3737b64c34ffffea30d12bab2
shomiyu <m1011522@gmail.com>
2024-08-03 20:54:34 +0900

    handlebarsの導入

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import globule from 'globule';
 import viteImagemin from 'vite-plugin-imagemin';
 import inject from '@rollup/plugin-inject';
+import handlebars from 'vite-plugin-handlebars';
 
 // ディレクトリ
 const dir = {
   src: 'src',
   publicDir: '../public',
   outDir: '../dist',
+};
+
+// 設定ファイル
+const configs = {
+  global: {},
+  meta: {},
+};
+
+// JSONファイルを読み込む関数
+const readConfigJSONFile = (filePath) => {
+  return new Promise(async (resolve) => {
+    const file = fs.readFileSync(filePath);
+    resolve(JSON.parse(file));
+  });
 };
 
 // マルチページ
@@ -36,6 +52,20 @@ export default defineConfig({
     inject({
       $: 'jquery',
       jQuery: 'jquery',
+    }),
+    handlebars({
+      context: async (pagePath) => {
+        configs.global = await readConfigJSONFile('./src/configs/global.json');
+        configs.meta = await readConfigJSONFile('./src/configs/meta.json');
+
+        return {
+          ...configs.global,
+          page:
+            typeof configs.meta[pagePath] !== 'undefined' &&
+            configs.meta[pagePath],
+          pagePath: pagePath,
+        };
+      },
     }),
     viteImagemin({
       gifsicle: {
